4622d3a97f1ecfe4f9a3db6920821a6cd85431ae
ari wibowo <ariwibowo.edu@gmail.com>
2025-08-24 10:50:19 +0700

    create biography of ari wibowo

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,81 @@
 <script setup>
+import { ref } from "vue";
+
+const profile = "Ari Wibowo";
 const timeline = [
-  { year: 1901, event: "Kelahiran di Surabaya, Indonesia" },
+  { year: 1999, event: "Kelahiran di Lampung, Indonesia." },
   {
-    year: 1915,
-    event: "Menyelesaikan sekolah dasar di Europeesche Lagere School (ELS)",
+    year: 1910,
+    event: "Menyelesaikan sekolah dasar di MIN 3 Lampung Barat.",
   },
   {
-    year: 1921,
+    year: 2014,
     event:
-      "Menyelesaikan sekolah menengah Belanda di Hogere Burger School (HBS)",
+      "Menyelesaikan sekolah menengah Pertama di Madrasah Tsanawiyah Darush sholihin Lampung Barat.",
   },
   {
-    year: 1926,
+    year: 2017,
     event:
-      "Lulus teknik sipil di Technische Hoogeshchool te Bandoeng (sekarang ITB)",
+      "Menyelesaikan sekolah menengah Atas di Madrasah Aliyah Darus sholihin Lampung Barat.",
   },
-  { year: 1927, event: "Mendirikan Partai Nasional Indonesia (PNI)" },
-  { year: 1929, event: "Ditangkap dan dipenjara di Sukamiskin Bandung" },
   {
-    year: 1930,
-    event: "Membacakan pidato 'Indonesia Menggugat' di Landraad Bandung",
+    year: 2017,
+    event: "Admin penjualan | Handala Sweet Ice Cream Depok.",
   },
-  { year: 1933, event: "Dibuang ke Ende, Flores" },
-  { year: 1942, event: "Bebas dari pengasingan, aktif di BPUPKI" },
-  { year: 1945, event: "Dilantik sebagai Presiden pertama Republik Indonesia" },
-  { year: 1967, event: "Menjadi tahanan rumah di Bogor" },
-  { year: 1970, event: "Wafat di Jakarta" },
+  {
+    year: 2018,
+    event:
+      "Satuan Pengamanan | PT. Bhawata Nusa Surya Perdana | client(PT. Bank Central Asia).",
+  },
+  {
+    year: 2020,
+    event:
+      "Satuan Pengamanan | PT. Garda Utama Nasional | client(PT. Bank Central Asia).",
+  },
+  {
+    year: 2024,
+    event:
+      "Full Stack Web Developer (magang) | CV. Krista Artha Sentosa (fit place).",
+  },
+  {
+    year: 2025,
+    event: "Menyelesaikan studi S1 Sistem Informasi di Universitas Terbuka.",
+  },
+  {
+    year: 2025,
+    event: "Junior Financial Consultant | Royal Trust Futures.",
+  },
 ];
 </script>
 
 <template class="max-w-4xl mx-auto flex flex-col items-center justify-center bg-white shadow-lg overflow-hidden">
   <header
-    class="w-full text-center p-10 bg-gradient-to-r from-red-600 to-red-400 text-white"
+    class="w-full text-center p-10 bg-gradient-to-r from-slate-800 to-gray-700 text-white"
   >
-    <h1 class="text-4xl font-extrabold mb-2">Ir. Soekarno</h1>
-    <p class="text-lg font-medium italic">Presiden pertama Indonesia</p>
+    <h1 class="text-4xl font-extrabold mb-2">{{ profile }}</h1>
+    <p class="text-lg font-medium italic">Web Dev</p>
   </header>
   <main class="p-8 space-y-8">
     <figure class="flex flex-col items-center text-center">
       <img
         class="rounded-xl shadow-md mb-4 border"
         width="200"
-        src="/images/Presiden_Sukarno.webp"
+        src="/images/profile_ariwibowo.webp"
         alt="presiden soekarno"
       />
       <figcaption class="text-sm text-gray-400">
-        Oleh Onbekend/Anonymous -
+        Oleh -
         <a
-          rel="nofollow"
+          target="_blank"
           class="external text"
-          href="http://hdl.handle.net/1887.1/item:782159"
-          >KITLV 2691</a
-        >, Domain Publik,
-        <a href="https://commons.wikimedia.org/w/index.php?curid=9780791"
-          >Pranala</a
-        >
+          href="https://instagram.com/aariiwiiboowoo"
+          >@aariiwiiboowoo
+        </a>
       </figcaption>
     </figure>
     <section id="tribute-info">
       <h2 class="text-2xl font-bold text-center mb-6">
-        Linimasa kehidupan Ir. Soekarno
+        Linimasa kehidupan {{ profile }}
       </h2>
       <ul
         class="space-y-4 border-l-4 border-red-500 pl-6 max-w-2xl mx-auto text-justify"
@@ -70,16 +86,6 @@ const timeline = [
       </ul>
     </section>
     <footer class="text-center pt-6 border-t mt-6">
-      <p class="text-gray-600">
-        Baca lebih lengkap kisahnya di
-        <a
-          href="https://id.wikipedia.org/wiki/Soekarno"
-          target="_blank"
-          class="text-blue-500 font-semibold underline"
-        >
-          wikipedia</a
-        >
-      </p>
       <p class="text-gray-400 text-md font-light">
         Made with ❤️ . by
         <a
